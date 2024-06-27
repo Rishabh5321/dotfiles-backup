07be86f9fcba91105ba36f681230db67f285df16
Rishabh5321 <rishabh98818@outlook.com>
2024-06-27 16:28:09 +0530

    for redmi

---
+++ hosts/redmi/variables.nix
@@ -0,0 +1,18 @@
+{
+  # Git Configuration ( For Pulling Software Repos )
+  gitUsername = "Rishabh5321";
+  gitEmail = "rishabh98818@outlook.com";
+
+  # Hyprland Settings
+  extraMonitorSettings = "";
+
+  # Waybar Settings
+  clock24h = false;
+
+  # Program Options
+  browser = "firefox"; # Set Default Browser (google-chrome-stable for google-chrome)
+  terminal = "kitty"; # Set Default System Terminal
+
+  theShell = "zsh";
+  host = "dell";
+}
