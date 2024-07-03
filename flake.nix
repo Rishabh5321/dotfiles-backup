90123e8e509de76761740b26e33a6ff849ecc03f
Rishabh5321 <40533251+Rishabh5321@users.noreply.github.com>
2024-07-03 11:10:45 +0530

    added walls

--- flake.nix
+++ flake.nix
@@ -31,7 +31,7 @@
       system = "x86_64-linux";
       host = "dell";
       username = "rishabh";
-      wallpaper = "wall3.jpg";
+      wallpaper = "wall45.jpg";
     in
     {
       nixosConfigurations = {
