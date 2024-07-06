2614f546d89f83acfed897d924262f788d884fdb
Rishabh5321 <rishabh98818@outlook.com>
2024-07-06 14:55:10 +0530

    added wallpapers

--- flake.nix
+++ flake.nix
@@ -31,7 +31,7 @@
       system = "x86_64-linux";
       host = "dell";
       username = "rishabh";
-      wallpaper = "wall44.jpg";
+      wallpaper = "wall35.jpg";
     in
     {
       nixosConfigurations = {
