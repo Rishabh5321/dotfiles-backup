86549432f2cca07d07ada4e453411de20d85dd12
Rishabh5321 <rishabh98818@outlook.com>
2024-07-06 15:18:47 +0530

    update

--- flake.nix
+++ flake.nix
@@ -31,7 +31,7 @@
       system = "x86_64-linux";
       host = "dell";
       username = "rishabh";
-      wallpaper = "wall35.jpg";
+      wallpaper = "wall53.jpg";
     in
     {
       nixosConfigurations = {
