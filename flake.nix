2c16dacdc7c4cec542151d05c2ed1af752056d63
Rishabh5321 <rishabh98818@outlook.com>
2024-07-09 12:22:17 +0530

    changed wall test

--- flake.nix
+++ flake.nix
@@ -31,7 +31,7 @@
       system = "x86_64-linux";
       host = "dell";
       username = "rishabh";
-      wallpaper = "wall9.jpg";
+      wallpaper = "wall20.png";
       pkgs-unstable = import nixpkgs-unstable {
       system = "x86_64-linux";
       config = {
