52fbe4f47c86025a8d6363b74e510b8656baaa89
Rishabh5321 <rishabh98818@outlook.com>
2024-07-11 20:10:17 +0530

    reset my laptop

--- changes/system/pkgs/cursor.nix
+++ changes/system/pkgs/cursor.nix
@@ -1,7 +1,7 @@
 # Code from https://gist.github.com/lukalot/fcbf3216ad13b8303ab0947af0d5abd5
 {pkgs, ...}: let
   pname = "cursor";
-  version = "0.35.1";
+  version = "0.36.2";
 
   src = pkgs.fetchurl {
     # this will break if the version is updated.
@@ -10,7 +10,7 @@
     # alternatively, download the appimage manually and
     # include it via src = ./cursor.AppImage, instead of fetchurl
     url = "https://downloader.cursor.sh/linux/appImage/x64";
-    hash = "sha256-Wo0mHeOQRo3WxAOXb2OT7h01eZv7dbAyGi6XNsCVtbg=";
+    hash = "sha256-4WW3/c15Wmf4P5XarNIBjjxPx55ixjKqg8sLUkq43vY=";
   };
   appimageContents = pkgs.appimageTools.extract {inherit pname version src;};
 in
