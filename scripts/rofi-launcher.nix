f4f84fce6d8d12fe7dc50b723627f384ec6657d6
Rishabh5321 <rishabh98818@outlook.com>
2024-07-15 19:04:46 +0530

    rofi script changes

--- scripts/rofi-launcher.nix
+++ scripts/rofi-launcher.nix
@@ -5,5 +5,11 @@ pkgs.writeShellScriptBin "rofi-launcher" ''
     pkill -x rofi
     exit 0
   fi
+
+  # Ensure Rofi is running with Wayland support
+  export GDK_BACKEND=wayland
+  export QT_QPA_PLATFORM=wayland
+
+  # Launch Rofi with layer shell protocol
   rofi -show drun
 ''
