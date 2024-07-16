f254427929531a72212fa42c3450a954114672f7
Rishabh5321 <rishabh98818@outlook.com>
2024-07-16 20:16:57 +0530

    syncthing network

--- changes/system/syncthing.nix
+++ changes/system/syncthing.nix
@@ -9,4 +9,8 @@
       configDir = "/home/${username}/Documents/.config/syncthing"; # Folder for Syncthing's settings and keys
     };
   };
+
+  networking.firewall.allowedTCPPorts = [ 8384 22000 ];
+  networking.firewall.allowedUDPPorts = [ 22000 21027 ];
+
 }
