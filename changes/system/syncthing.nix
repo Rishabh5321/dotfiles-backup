51b46c5f55ad754e310ae3ac2c8105a2a21d9420
Rishabh5321 <Rishabh5321@users.noreply.github.com>
2024-07-16 14:33:02 +0000

    Auto lint/format

--- changes/system/syncthing.nix
+++ changes/system/syncthing.nix
@@ -1,12 +1,12 @@
-{username ,...}:
+{ username, ... }:
 {
 
-services = {
+  services = {
     syncthing = {
-        enable = true;
-        user = "$username";
-        dataDir = "/home/$username/";    # Default folder for new synced folders
-        configDir = "/home/$username/Documents/.config/syncthing";   # Folder for Syncthing's settings and keys
+      enable = true;
+      user = "$username";
+      dataDir = "/home/$username/"; # Default folder for new synced folders
+      configDir = "/home/$username/Documents/.config/syncthing"; # Folder for Syncthing's settings and keys
     };
-};
+  };
 }
