d64bbc5e2da67a38ff48103b103925ebccd6bafa
Rishabh5321 <Rishabh5321@users.noreply.github.com>
2024-07-16 14:50:06 +0000

    Auto lint/format

--- changes/system/syncthing.nix
+++ changes/system/syncthing.nix
@@ -10,7 +10,12 @@
     };
   };
 
-  networking.firewall.allowedTCPPorts = [ 8384 22000 ];
-  networking.firewall.allowedUDPPorts = [ 22000 21027 ];
-
+  networking.firewall.allowedTCPPorts = [
+    8384
+    22000
+  ];
+  networking.firewall.allowedUDPPorts = [
+    22000
+    21027
+  ];
 }
