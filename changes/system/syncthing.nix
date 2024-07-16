6c86b6fa24f7e03b2ce91012a45f2c06fe24c794
Rishabh5321 <rishabh98818@outlook.com>
2024-07-16 20:07:20 +0530

    syncing home folder

--- changes/system/syncthing.nix
+++ changes/system/syncthing.nix
@@ -4,9 +4,9 @@
   services = {
     syncthing = {
       enable = true;
-      user = "$username";
-      dataDir = "/home/$username/"; # Default folder for new synced folders
-      configDir = "/home/$username/Documents/.config/syncthing"; # Folder for Syncthing's settings and keys
+      user = "${username}";
+      dataDir = "/home/${username}/"; # Default folder for new synced folders
+      configDir = "/home/${username}/Documents/.config/syncthing"; # Folder for Syncthing's settings and keys
     };
   };
 }
