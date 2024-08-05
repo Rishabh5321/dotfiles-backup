989a5bf5215cef086d2a02d287ad325ffc4a7236
Rishabh5321 <rishabh98818@outlook.com>
2024-08-05 20:28:31 +0530

    workflow updates

--- hosts/dell/home.nix
+++ hosts/dell/home.nix
@@ -44,7 +44,7 @@
   home.file.".config/swappy/config".text = ''
     [Default]
     save_dir=/home/${username}/Pictures/Screenshots
-    save_filename_format=swappy-%Y%m%d-%H%M%S.png
+    save_filename_format=screenshot-%Y%m%d-%H%M%S.png
     show_panel=false
     line_size=5
     text_size=20
